da3e5f58b8d55c5fcd34cdfb35a1a3a5c786160d
tkw_yone <tkw_yoneta@outlook.com>
2023-04-11 14:00:19 +0900

    qt_training_7

---
+++ 110qt.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import cv2
+import numpy as np
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+class VideoPlayer(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setFocusPolicy(Qt.StrongFocus) # キーボードフォーカスを設定
+
+        # ビデオキャプチャーオブジェクトの作成
+        self.cap = cv2.VideoCapture()
+
+        # UI要素の作成
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(5,5,1280,720)
+        self.filename_label1 = QtWidgets.QLabel(self)
+        self.filename_label1.move(5, 900)
+        self.filename_label2 = QtWidgets.QLabel(self)
+        self.filename_label2.move(10, 1000)
+
+        # ウィンドウの設定
+        xPos = 0  # x座標
+        yPos = 0  # y座標
+        windowWidth = 1920   # ウィンドウの横幅
+        windowHeight = 1080  # ウィンドウの高さ
+        self.setGeometry(xPos, yPos, windowWidth, windowHeight)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setStyleSheet("background-color: yellow;")
+
+        # タイマーの作成
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.play)
+
+
+    def load_video(self, filename):
+        # ビデオをロードして再生する準備をする
+        filename = ("./dualHA_BSH2.mp4")
+        self.cap.open(filename)
+        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+        self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.current_frame = 0
+        self.filename_label1.setText(filename)
+        self.filename_label2.setText(filename)
+
+        # タイマーを開始
+        self.timer.start(1000/self.fps)
+
+    def play(self):
+        # ビデオフレームを読み取り、表示する
+        ret, frame = self.cap.read()
+        if ret:
+            self.current_frame += 1
+            if self.current_frame >= self.total_frames:
+                self.current_frame = 0
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = QtGui.QImage(frame, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
+            pixmap = QtGui.QPixmap.fromImage(image)
+            self.label.setPixmap(pixmap)
+        else:
+            # ビデオ再生が終了したらタイマーを停止する
+            self.timer.stop()
+            self.cap.release()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape: # Escapeキーが押された場合
+            self.close() # アプリケーションを終了
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    player = VideoPlayer()
+    player.load_video('')
+    player.show()
+    sys.exit(app.exec_())
+
--- widget001_ui.py
+++ widget001_ui.py
@@ -15,20 +15,31 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(400, 300)
+        Form.resize(1920, 1080)
+        Form.setMinimumSize(QSize(1920, 1080))
+        Form.setMaximumSize(QSize(1920, 1080))
+        Form.setSizeIncrement(QSize(1920, 1080))
+        Form.setBaseSize(QSize(1920, 1080))
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 10, 131, 111))
+        self.pushButton.setAutoFillBackground(False)
+        self.pushButton.setIconSize(QSize(16, 16))
 
         self.retranslateUi(Form)
+        self.pushButton.clicked.connect(Form.close)
 
         QMetaObject.connectSlotsByName(Form)
     # setupUi
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"end", None))
     # retranslateUi
 
